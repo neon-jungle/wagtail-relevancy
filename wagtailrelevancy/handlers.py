95d697117791d08147e8254d8cf117b4e5dd6104
Liam Brenner <liam.brenner@gmail.com>
2016-01-05 13:14:12 +1100

    Ability (default) to remove old reminders, signal handler for when pages are unpublished.

--- wagtailrelevancy/handlers.py
+++ wagtailrelevancy/handlers.py
@@ -1,14 +1,15 @@
-import datetime
+from datetime import timedelta
 
 from django.conf import settings
 from django.dispatch import receiver
-from wagtail.wagtailcore.signals import page_published
+from django.utils import timezone
+from wagtail.wagtailcore.signals import page_published, page_unpublished
 
 from .models import Reminder
 
 
 @receiver(page_published)
-def send_reminders(sender, **kwargs):
+def create_reminders(sender, **kwargs):
     instance = kwargs['instance']
     revision = kwargs['revision']
     user = revision.user
@@ -18,16 +19,24 @@ def send_reminders(sender, **kwargs):
     except AttributeError:
         remind_interval = 14
 
-    due_to_be_sent_at = datetime.datetime.now() + datetime.timedelta(days=remind_interval)
+    due_to_be_sent_at = timezone.now() + timedelta(days=remind_interval)
 
     try:
         old_reminder = Reminder.objects.get(page=instance, sent=False)
         old_reminder.due_to_be_sent_at = due_to_be_sent_at
         old_reminder.save()
-        print('did this')
     except Reminder.DoesNotExist:
         Reminder.objects.create(
             page=instance,
             user=user,
             due_to_be_sent_at=due_to_be_sent_at
         )
+
+
+@receiver(page_unpublished)
+def check_reminders(sender, **kwargs):
+    instance = kwargs['instance']
+
+    reminders = Reminder.objects.filter(page=instance)
+    print('Removing {0} reminders, as the page {1} has been unpublished.'.format(reminders.count(), instance.title))
+    reminders.delete()
--- wagtailrelevancy/management/commands/reminders.py
+++ wagtailrelevancy/management/commands/reminders.py
@@ -1,9 +1,10 @@
-import datetime
+from datetime import timedelta
 
 from django.conf import settings
 from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
+from django.utils import timezone
 from wagtailrelevancy.models import Reminder
 
 
@@ -22,9 +23,7 @@ class Command(BaseCommand):
             reminder_template = 'wagtailrelevancy/reminder.html'
 
         for reminder in reminders:
-            print(reminder.due_to_be_sent_at.day)
-            print(datetime.datetime.now().day)
-            if reminder.due_to_be_sent_at.day == datetime.datetime.now().day:
+            if reminder.due_to_be_sent_at.day == timezone.now().day:
                 subject = 'Reminder: Check to see if you content on your page, {0}, is up to date!'.format(reminder.page.title)
                 message = render_to_string(
                     reminder_template, {
@@ -43,3 +42,18 @@ class Command(BaseCommand):
 
                 reminder.sent = True
                 reminder.save()
+
+        # Purge old sent reminders after 14 days, unless set to false in settings
+        try:
+            purge = settings.PURGE_REMINDERS
+        except AttributeError:
+            purge = True
+
+        if purge:
+            fourteen_days_after_now = timezone.now() + timedelta(days=14)
+            stale_reminders = Reminder.objects.filter(sent=True, due_to_be_sent_at__gt=fourteen_days_after_now)
+            if stale_reminders.exists():
+                print('Purging {0} stale reminders'.format(stale_reminders.count()))
+                stale_reminders.delete()
+            else:
+                print('There were no stale reminders to purge.')
